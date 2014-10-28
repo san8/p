d8da2e57dd619cdc66e8bef8829c291f40a7ef0a
K3 <anand.r@leucinerichbio.com>
2014-10-28 11:28:29 +0530

    added production settings; removed staging, test settings

---
+++ pearl/pearl/settings/production.py
@@ -0,0 +1,57 @@
+"""Additional settings for Development Environment."""
+
+import os 
+
+from .base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1',]
+
+INSTALLED_APPS += (
+#    'debug_toolbar',
+    'django_jenkins',
+    'django_shell_ipynb',
+    'django_extensions',
+    #'djcelery_testworker',
+)
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pearl',
+        'USER': 'root',
+        'PASSWORD': 'bacillus123',
+        'HOST': 'localhost',   
+        'PORT': '3306',
+    }
+}
+
+#import dj_database_url
+#DATABASES['default'] = dj_database_url.config(default='mysql://db/pearl.db')
+
+# django-nose
+#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# djceley testrunner
+TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
+
+
+#LOGIN_URL = 'accounts/login'
+#LOGIN_REDIRECT_URL = 'accounts/dashboard'
+SECRET_KEY='@-+a#8cz=c^&jjf9ai!cw976%k!+@v3!%l+j9e%-d#h466*i'
+
+
+EMAIL_HOST = 'mail.leucinerichbio.com'
+EMAIL_PORT = 26
+EMAIL_HOST_USER = 'noreply@leucinerichbio.com'
+EMAIL_HOST_PASSWORD = 'reply123'
