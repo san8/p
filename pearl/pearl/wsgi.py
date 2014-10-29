7c7eedca7f1899e10266058d1bede5943bfdff54
K3 <anand.r@leucinerichbio.com>
2014-10-29 11:11:01 +0530

    added sys path to wsgi file

--- pearl/pearl/wsgi.py
+++ pearl/pearl/wsgi.py
@@ -10,5 +10,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pearl.settings")
 
+import sys
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
