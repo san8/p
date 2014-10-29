3c373bff3ec8c86bf828cc595ef5e12289114f1f
K3 <anand.r@leucinerichbio.com>
2014-10-29 14:45:39 +0530

    added workflow.pl to process fastq files

--- pearl/apps/processing/tasks.py
+++ pearl/apps/processing/tasks.py
@@ -1,15 +1,23 @@
-import sys
+"""
+Various tasks to process the user files.
+"""
 
+import sys
+import os
+import subprocess
+
+from pearl.settings.base import NEW_PROJECT_DIR
 from pearl.celery_conf import app as celery_app
 
 
 @celery_app.task()
 def processing(project_id, file_type):
-    import time
-    time.sleep(10)
+    """
+    Route tasks according to their file type.
+    """
     if file_type == 'fastq':
-        fastq_processing(project_id)
-    else:
+        fastq_processing.delay(project_id)
+    elif file_type == 'vcf':
         vcf_processing.apply_async(args=[project_id,])
     return True
 
@@ -17,10 +25,19 @@ def processing(project_id, file_type):
 @celery_app.task()
 def fastq_processing(project_id):
     """
-    Do fastq processing.
+    Run fastq through a perl script which runs all the pipelines.
     """
-    update_status(project_id, 4)
-    print(sys._getframe().f_code.co_name)
+    project_dir = os.path.join(NEW_PROJECT_DIR, str(project_id)) 
+    print(project_dir)
+    fq_files = [ os.path.join(project_dir, f) for f in os.listdir(project_dir) \
+                 if f[-6:] == '.fastq' ]
+    if len(fq_files) == 2:
+        command = ["pearl workflow.pl ", "-1", fq_files[0], "-2", fq_files[1],
+                   "-o", project_dir]
+    if len(fq_files) == 1:
+        command = ["perl workflow.pl", "-u", fq_files[0], "-o", project_dir]
+    subprocess.call(command, stdout=open(os.devnull, 'wb'))
+    return True
 
 
 @celery_app.task()
