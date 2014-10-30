0b757d236e5f4786bb94b33b5212480722bc9c6c
K3 <anand.r@leucinerichbio.com>
2014-10-30 15:42:15 +0530

    removed imports from wsgi

--- pearl/pearl/wsgi.py
+++ pearl/pearl/wsgi.py
@@ -1,17 +1,9 @@
 """
 WSGI config for pearl project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pearl.settings")
-
-import sys
-sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
