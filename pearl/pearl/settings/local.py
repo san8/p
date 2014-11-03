d95fd534ee7ce16b7a4afe32434e1351a0107478
K3 <anand.r@leucinerichbio.com>
2014-11-03 12:56:10 +0530

    modified local & production settings

--- pearl/pearl/settings/local.py
+++ pearl/pearl/settings/local.py
@@ -1,10 +1,12 @@
-"""Additional settings for Development Environment."""
+"""
+Additional settings for Development Environment.
+"""
 
 import os 
 
 from .base import *
 
-SECRET_KEY='@-+a#8cz=c^&jjf9ai!cw976%k!+@v3!%l+j9e%-d#h466*i'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 DEBUG = True
 
@@ -14,10 +16,8 @@ ALLOWED_HOSTS = ['127.0.0.1',]
 
 INSTALLED_APPS += (
     'debug_toolbar',
-    #'django_jenkins',
-    #'django_shell_ipynb',
     'django_extensions',
-    #'djcelery_testworker',
+    'djcelery_testworker',
 )
 
 # Database
@@ -25,14 +25,21 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pearl',
-        'USER': 'root',
-        'PASSWORD': 'pearl',
-        'HOST': 'localhost',   
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
 
-# djceley testrunner
+# djcelery testrunner
 TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
 
 
+# email settings
+EMAIL_HOST = 'mail.leucinerichbio.com'
+EMAIL_PORT = 26
+EMAIL_HOST_USER = 'noreply@leucinerichbio.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS', '')
+DEFAULT_FROM_EMAIL = 'noreply@leucinerichbio.com'
+DEFAULT_TO_EMAIL = 'noreply@leucinerichbio.com'
--- pearl/pearl/settings/production.py
+++ pearl/pearl/settings/production.py
@@ -1,41 +1,40 @@
-"""Additional settings for Development Environment."""
+"""
+Additional settings for Production Environment.
+"""
 
 import os 
 
 from .base import *
 
-SECRET_KEY='@-+a#8cz=c^&jjf9ai!cw976%k!+@v3!%l+j9e%-d#h466*i'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['192.168.0.103', '127.0.0.1']
 
 INSTALLED_APPS += (
     #'debug_toolbar',
-    #'django_jenkins',
-    #'django_shell_ipynb',
-    'django_extensions',
-    #'djcelery_testworker',
 )
 
+# Database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pearl',
-        'USER': 'root',
-        'PASSWORD': 'bacillus123',
-        'HOST': 'localhost',   
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASS', ''),
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
 
-# djceley testrunner
-TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
 
-
+# email settings
 EMAIL_HOST = 'mail.leucinerichbio.com'
 EMAIL_PORT = 26
 EMAIL_HOST_USER = 'noreply@leucinerichbio.com'
-EMAIL_HOST_PASSWORD = 'reply123'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS', '')
+DEFAULT_FROM_EMAIL = 'noreply@leucinerichbio.com'
+DEFAULT_TO_EMAIL = 'noreply@leucinerichbio.com'
