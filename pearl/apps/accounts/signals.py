16cc7acedefdc4a8abe81f3b3e5208b3194cc62f
K3 <anand.r@leucinerichbio.com>
2014-11-21 12:22:19 +0530

    send mail to info@lrb when someone registers

--- pearl/apps/accounts/signals.py
+++ pearl/apps/accounts/signals.py
@@ -1,4 +1,6 @@
 from django.dispatch import receiver
+from django.core.mail import send_mail
+
 from registration.signals import user_registered
 
 
@@ -13,4 +15,15 @@ def user_registered_callback(sender, user, request, **kwargs):
     customer.phone_number = form.data["phone_number"]
     customer.save()
 
+    # send additional mail to info
+    message = """
+    User details:
+    Name: {0}
+    Email: {1}
+    Phone Number: {2}
+    """.format(user.first_name, user.email, customer.phone_number)
+    
+    send_mail('New User Has Registered', message,
+              'noreply@leucinerichbio.com',
+              ['info@leucinerichbio.com', 'anand21nanda@gmail.com', ])
 
