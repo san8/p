8e557b50723b8f6fe0cf23020d56b5515c04bb4f
K3 <anand.r@leucinerichbio.com>
2014-12-15 14:53:07 +0530

    added new field for accounts balance

--- pearl/apps/accounts/models.py
+++ pearl/apps/accounts/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
-from captcha.fields import CaptchaField
 
 #listener must be invoked before sending a signal
-from signals import user_registered_callback
+from .signals import user_registered_callback
 
 
 class Customer(models.Model):
+    """
+    Store customer details.
+    """
     user = models.ForeignKey(User, related_name='customer_to_user')
     name = models.CharField(max_length=50, blank=True)
     company = models.CharField(max_length=50, blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
     timezone = models.CharField(max_length=50, default='', blank=True)
+    balance = models.FloatField(default=0.0)
 
     def __unicode__(self):
         return self.name # pragma: no cover
