2966e77f856244323e0ad2ab3e87a6fd05efbd9f
K3 <anand.r@leucinerichbio.com>
2014-12-23 11:13:51 +0530

    removed unused urls from accounts

--- pearl/apps/accounts/urls.py
+++ pearl/apps/accounts/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url, patterns 
+from django.conf.urls import url, patterns
 from django.contrib.auth.decorators import login_required
 
-from .views import ProfileView 
+from .views import ProfileView
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
 
-    url(r'^profile', login_required(ProfileView.as_view()), name='account_profile'),
-    #url(r'^editprofile', EditProfile.as_view(), name='account_editprofile'),
-    #url(r'^signup', SignUpView.as_view(), name='accounts_signup'),
+    url(r'^profile/$', login_required(ProfileView.as_view()),
+        name='account_profile'),
 )
