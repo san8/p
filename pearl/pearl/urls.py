f0a7b6b8f394e4d67daad9117d2b03aa06bc9ca9
K3 <anand.r@leucinerichbio.com>
2014-12-23 11:10:44 +0530

    refactored pearl urls

--- pearl/pearl/urls.py
+++ pearl/pearl/urls.py
@@ -1,30 +1,29 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.conf import settings
 from django.conf.urls.static import static
-
-from pearl.settings.base import MEDIA_URL, MEDIA_ROOT
 
 from registration.backends.default.views import RegistrationView
 
+from pearl.settings.base import MEDIA_URL, MEDIA_ROOT
 from apps.home.views import HomeView
 from apps.accounts.forms import CustomerForm
 
+
 admin.autodiscover()
+pay_pal = get_integration("pay_pal")
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
 
-    url(r'^$', HomeView.as_view()),
     url(r'^home/', include('apps.home.urls', namespace='home')),
-    url(r'^account/', include('apps.accounts.urls', namespace='account')),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
                       {'next_page': '/home/'}),
-    url(r'^accounts/register/$',  RegistrationView.as_view(form_class=CustomerForm),
+    url(r'^accounts/register/$', RegistrationView.as_view(form_class=CustomerForm),
                                   name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
+    url(r'^account/', include('apps.accounts.urls', namespace='account')),
     url(r'^project/', (include('apps.project.urls', namespace='project'))),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
+    url(r'^$', HomeView.as_view()),
 ) + static(MEDIA_URL, document_root = MEDIA_ROOT)
-
