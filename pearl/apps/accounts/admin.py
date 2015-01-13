116f5c6591ed6c79a1a9ff3d15a12859a2d9844b
anand.r@leucinerichbio.com <anand.r@leucinerichbio.com>
2015-01-13 09:34:57 +0530

    added Discount model to admin

--- pearl/apps/accounts/admin.py
+++ pearl/apps/accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Customer, Payment
+from .models import Customer, Payment, Discount
 
 
 admin.site.register(Customer)
 admin.site.register(Payment)
+admin.site.register(Discount)
