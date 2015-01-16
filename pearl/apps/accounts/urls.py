f040e07a16d4e33ba2c15624b1f1cd10678a2c66
anand.r@leucinerichbio.com <anand.r@leucinerichbio.com>
2015-01-16 15:09:46 +0530

    added paymentcancelview to accounts

--- pearl/apps/accounts/urls.py
+++ pearl/apps/accounts/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url, patterns
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
-from .views import ProfileView, PaymentSuccessView, InsufficientBalanceView
+from .views import (ProfileView, PaymentSuccessView, InsufficientBalanceView,
+                    PaymentCancelView)
 
 
 urlpatterns = patterns(
@@ -11,7 +12,10 @@ urlpatterns = patterns(
     url(r'^profile/$', login_required(ProfileView.as_view()),
         name='account_profile'),
     url(r'^payment-success/$', csrf_exempt(login_required(PaymentSuccessView)),
-        name='payment'),
+        name='payment-success'),
+    url(r'^payment-cancel/$', login_required(PaymentCancelView),
+        name='payment-cancel'),
     url(r'^insufficient-balance/$', login_required(InsufficientBalanceView),
         name='insufficient_balance')
+
 )
