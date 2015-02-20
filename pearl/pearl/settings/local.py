dba913c590fa71c58d0edb53839b9907ad486615
Anand <nandapk0@gmail.com>
2015-02-20 10:46:32 +0530

    dev - get email settings from env variables

--- pearl/pearl/settings/local.py
+++ pearl/pearl/settings/local.py
@@ -33,10 +33,11 @@ DATABASES = {
     }
 }
 
-
-EMAIL_HOST = 'mail.leucinerichbio.com'
-EMAIL_PORT = 26
-EMAIL_HOST_USER = 'noreply@leucinerichbio.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS', '')
-DEFAULT_FROM_EMAIL = 'noreply@leucinerichbio.com'
-DEFAULT_TO_EMAIL = 'noreply@leucinerichbio.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+EMAIL_PORTHOST = os.environ.get('EMAIL_PORT', '')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', '')
+SECURITY_EMAIL_SENDER = os.environ.get('SECURITY_EMAIL_SENDER', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
+DEFAULT_TO_EMAIL = os.environ.get('DEFAULT_TO_EMAIL', '')
